267eba6a0e76157059ab6f02093dbb22e40b4ed7
James Sandridge <jims@radical-ideal.com>
2016-09-01 20:57:45 -0400

    Start posting status events to valhalla

--- workloads/app/Main.hs
+++ workloads/app/Main.hs
@@ -16,21 +16,34 @@ import Filesystem.Path
 import Filesystem.Path.CurrentOS
 import Prelude hiding (FilePath)
 import Twitch
+import Network.Wreq (post, responseStatus, statusCode)
+import Data.Aeson (toJSON, object, (.=), Value)
+import Control.Lens((^.))
 
 import Lib
+
+-- TODO: let's have a proper type of events so it's not an evil string!
+create_json :: String -> String -> Value
+create_json event_type file_name = object [(fromString "data") .= object [(fromString "event_type") .= event_type, (fromString "file_name") .= file_name]]
+
+postEvent event_type file_name = do
+  r <- post "http://localhost:3000/fsevents" (create_json event_type file_name)
+  print (r ^. responseStatus . statusCode)
 
 -- We have this combined 'upsert' since either twitch or the other underlying mechanisms
 -- will sometimes only return a 'modify' event when creating a new file
 handleAddOrMod :: String -> IO ()
 handleAddOrMod n = do
   print ("The following file was added or modified: " ++ n)
+  postEvent "addormod" n
 
 handleDel :: String -> IO ()
 handleDel d = do
   print ("The following file was deleted: " ++ d)
+  postEvent "delete" d
 
 opts = Twitch.Options NoLogger Nothing Nothing True NoDebounce 0 (10^(6 :: Int)) False
 
 main :: IO ()
 main = defaultMainWithOptions opts $ do
-  (fromString "*.*") |> handleAddOrMod |- handleDel
+  (fromString "*") |> handleAddOrMod |- handleDel
---
+++ workloads/runner.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd .stack-work/dist/x86_64-osx/Cabal-1.22.5.0/build/workloads-exe/
+./workloads-exe
