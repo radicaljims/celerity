0d4d7a41c6857d7793de83835497b3f85a0c1471
James Sandridge <jims@radical-ideal.com>
2016-09-04 19:01:59 -0400

    Clean up the file copying bits; now reliably moves file changes into a contextually appropriate place in the 'share'

--- workloads/app/Main.hs
+++ workloads/app/Main.hs
@@ -12,16 +12,14 @@ import Data.String
 import Control.Applicative
 import System.Process
 import Control.Monad
--- import Filesystem.Path
--- import Filesystem.Path.CurrentOS
 import Prelude hiding (FilePath)
 import Twitch
 import Network.Wreq (post, responseStatus, statusCode)
 import Data.Aeson (toJSON, object, (.=), Value)
 import Control.Lens((^.))
 
-import Turtle
-import System.FilePath
+import Turtle hiding (empty)
+import Filesystem.Path.CurrentOS as OS
 
 import Lib
 
@@ -29,18 +27,31 @@ import Lib
 create_json :: String -> String -> Value
 create_json event_type file_name = object [(fromString "data") .= object [(fromString "eventType") .= event_type, (fromString "fileName") .= file_name]]
 
--- TODO: need to consider the possible error modes here
+-- TODO: need to consider the possible error modes here (and everywhere else!)
 postEvent event_type file_name = do
   r <- post "http://localhost:3000/fsevents" (create_json event_type file_name)
   print (r ^. responseStatus . statusCode)
+
+-- TODO: share and directory to watch in need to be command line arguments
+copyToShare :: String -> IO ()
+copyToShare path = do
+  let filepath = (fromString path)
+      name = (filename filepath)
+      shareprefix = (fromString "/Users/jims/share")
+      -- this lovely bit has the effect of stripping off the 'drive' and the file parts of filepath,
+      -- relativizing the directory and letting us move it under our share
+      directorypath = shareprefix </> (foldr (</>) OS.empty (tail $ init $ splitDirectories $ directory filepath))
+      sharepath = (shareprefix </> directorypath </> name) in
+        do
+          mktree directorypath
+          cp filepath sharepath
 
 -- We have this combined 'upsert' since either twitch or the other underlying mechanisms
 -- will sometimes only return a 'modify' event when creating a new file
 handleAddOrMod :: String -> IO ()
 handleAddOrMod n = do
   print ("The following file was added or modified: " ++ n)
-  print (takeFileName (fromString n))
-  cp (fromString n) (fromString "/Users/jims/" Turtle.</> (fromString (takeFileName n)))
+  copyToShare n
   postEvent "addormod" n
 
 handleDel :: String -> IO ()
