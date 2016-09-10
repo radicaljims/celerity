cf22c1d43d85efdd888ec6f56e5a546809f4882b
James Sandridge <jims@radical-ideal.com>
2016-09-10 15:57:07 -0400

    Insert stub Servant module

---
+++ workloads/indy/app/Main.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Main where
+
+import Lib
+
+import Prelude ()
+import Prelude.Compat
+
+import Control.Monad.Except
+import Control.Monad.Reader
+import Data.Aeson.Compat
+import Data.Aeson.Types
+import Data.Attoparsec.ByteString
+import Data.ByteString (ByteString)
+import Data.List
+import Data.Maybe
+import Data.String.Conversions
+import Data.Time.Calendar
+import GHC.Generics
+import Network.HTTP.Media ((//), (/:))
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+import System.Directory
+import qualified Data.Aeson.Parser
+
+
+type UserAPI1 = "users" :> Get '[JSON] [User]
+
+data User =
+  User {name :: String
+       ,age :: Int
+       ,email :: String
+       ,registration_date :: Day}
+  deriving (Eq,Show,Generic)
+
+instance ToJSON User
+
+users1 :: [User]
+users1 =
+  [User "Isaac Newton" 372 "isaac@newton.co.uk" (fromGregorian 1683 3 1)
+  ,User "Albert Einstein" 136 "ae@mc2.org" (fromGregorian 1905 12 1)]
+
+server1 :: Server UserAPI1
+server1 = return users1
+
+userAPI :: Proxy UserAPI1
+userAPI = Proxy
+
+-- 'serve' comes from servant and hands you a WAI Application,
+-- which you can think of as an "abstract" web application,
+-- not yet a webserver.
+app1 :: Application
+app1 = serve userAPI server1
+
+main :: IO ()
+main = run 8081 app1
