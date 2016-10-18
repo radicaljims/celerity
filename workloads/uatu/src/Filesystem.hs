d5886a98d0e8129cbdceec9d075dc21d9355ece4
James Sandridge <jims@radical-ideal.com>
2016-10-18 00:36:30 -0400

    Locate copied files under timestamped folder

--- workloads/uatu/src/Filesystem.hs
+++ workloads/uatu/src/Filesystem.hs
@@ -8,6 +8,7 @@ module Filesystem(watchForEvents) where
 {-# LANGUAGE QuasiQuotes      #-}
 
 import Data.String
+import Data.Time.Clock.POSIX
 import Control.Applicative
 import System.Process
 import Control.Monad
@@ -19,19 +20,30 @@ import Filesystem.Path.CurrentOS as OS
 
 import Valhalla
 
+-- NOTE: we only support copying files in watched directories, not subdirectories!
+
+-- getTime = show $ posixSecondsToUTCTime $ getPOSIXTime
+getTime = do
+  posix <- getPOSIXTime
+  return (show $ (posixSecondsToUTCTime posix))
+
 -- TODO: share and directory to watch in need to be command line arguments
 copyToShare :: String -> IO ()
 copyToShare path = do
-  let filepath = (fromString path)
-      name = (filename filepath)
-      shareprefix = (fromString "/Users/jims/share")
-      -- this lovely bit has the effect of stripping off the 'drive' and the file parts of filepath,
-      -- relativizing the directory and letting us move it under our share
-      directorypath = shareprefix </> (foldr (</>) OS.empty (tail $ init $ splitDirectories $ directory filepath))
-      sharepath = (shareprefix </> directorypath </> name) in
-        do
-          mktree directorypath
-          cp filepath sharepath
+  timestamp <- getTime
+  let
+    -- TODO: would be better to get the mtime of the path, probably...
+    filepath = (fromString path)
+    name = (filename filepath) -- WARNING: we aren't stripping directories out, they could blow up here!
+    shareprefix = (fromString "/Users/jims/share")
+    -- this lovely bit has the effect of stripping off the 'drive' and the file parts of filepath,
+    -- relativizing the directory and letting us move it under our share
+    directorypath = shareprefix </> (foldr (</>) OS.empty (tail $ init $ splitDirectories $ directory filepath))
+    sharepath = (shareprefix </> directorypath </> (fromString timestamp) </> name)
+    in
+      do
+        mktree directorypath
+        cp filepath sharepath
 
 -- We have this combined 'upsert' since either twitch or the other underlying mechanisms
 -- will sometimes only return a 'modify' event when creating a new file
